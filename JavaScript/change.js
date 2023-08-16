0f600d0259bb77234375493c6acd86941fa10e04
kikura19 <github118703694+kikura19@users.noreply.github.com>
2023-08-17 00:05:34 +0900

    画面の切り替え方法を変えて、切り替えボタンをなくした

--- JavaScript/change.js
+++ JavaScript/change.js
@@ -1,23 +1,9 @@
-const tohome = document.getElementById("tohome");
 const tosetting = document.getElementById("tosetting");
 const home = document.getElementById("home");
 const setting = document.getElementById("setting");
 
 
-tohome.addEventListener("click",function(){
-    home.classList.toggle("none");
-    setting.classList.toggle("none");
-    tohome.classList.toggle("green");
-    tohome.classList.toggle("white");
-    tosetting.classList.toggle("green");
-    tosetting.classList.toggle("white");
-})
-
 tosetting.addEventListener("click",function(){
-    home.classList.toggle("none");
-    setting.classList.toggle("none");
-    tohome.classList.toggle("green");
-    tohome.classList.toggle("white");
-    tosetting.classList.toggle("green");
-    tosetting.classList.toggle("white");
+    home.classList.add("none");
+    setting.classList.remove("none");
 })
