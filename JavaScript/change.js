fedaee1eacd645027865020fadd7b2d0111a52a0
kikura19 <github118703694+kikura19@users.noreply.github.com>
2023-08-17 03:32:10 +0900

    ホームロゴを押しても、画面切り替えが画面遷移なしでいけるようにした

--- JavaScript/change.js
+++ JavaScript/change.js
@@ -1,3 +1,4 @@
+const tohome = document.getElementById("tohome");
 const tosetting = document.getElementById("tosetting");
 const home = document.getElementById("home");
 const setting = document.getElementById("setting");
@@ -6,4 +7,9 @@ const setting = document.getElementById("setting");
 tosetting.addEventListener("click",function(){
     home.classList.add("none");
     setting.classList.remove("none");
+})
+
+tohome.addEventListener("click",function(){
+    home.classList.remove("none");
+    setting.classList.add("none");
 })
