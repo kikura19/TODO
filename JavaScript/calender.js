bf1d074bb7f21d41fcbda86b825686ed912650e2
kikura19 <github118703694+kikura19@users.noreply.github.com>
2023-08-18 00:36:28 +0900

    今日の日付を押すと、カレンダーが表示されるようにした

---
+++ JavaScript/calender.js
@@ -0,0 +1,71 @@
+const YM = document.getElementById("y-m");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const calendar = document.getElementById("calender");
+const theDate = document.getElementById("date");
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+const today = new Date();
+
+let showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+window.onload = function(){
+    showProcess(today, calendar);
+    let y = today.getFullYear();
+    let m = today.getMonth();
+    let day = today.getDate();
+    let dayOfWeek = today.getDay();
+    theDate.innerHTML = y + "/" + (m + 1) + "/" + day + " (" + week[dayOfWeek] + ")";
+}
+
+prev.addEventListener("click",function(){
+    showDate.setMonth(showDate.getMonth() - 1);
+    showProcess(showDate);
+})
+next.addEventListener("click",function(){
+    showDate.setMonth(showDate.getMonth() + 1);
+    showProcess(showDate);
+})
+
+function showProcess(date){
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    YM.innerHTML = year + "年 " + (month + 1) + "月";
+    let calen = createProcess(year, month);
+    calendar.innerHTML = calen;
+}
+
+function createProcess(year, month){
+    let frame = "<table><tr class='dayOfWeek'>";
+    for(i = 0; i < week.length; i++){
+        frame += "<th>" + week[i] + "</th>";
+    }
+    frame += "</tr>";
+
+    let count = 0;
+    let startDayOfWeek = new Date(year, month, 1).getDay();
+    let endDate = new Date(year, month + 1, 0).getDate();
+    let lastMonthEndDate = new Date(year, month, 0).getDate();
+    let row = Math.ceil((startDayOfWeek + endDate)/ week.length);
+
+    for(i = 0; i < row; i++){
+        frame += "<tr>";
+
+        for(j = 0; j < week.length; j++){
+            if (i == 0 && j < startDayOfWeek){
+                frame += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>"; 
+            }else if (count >= endDate) {
+                count++;
+                frame += "<td class='disabled'>" + (count - endDate) + "</td>";
+            }else{
+                count++;
+                if(year == today.getFullYear() && month == (today.getMonth()) && count == today.getDate()){
+                    frame += "<td class='today'>" + count + "</td>";
+                }else{
+                    frame += "<td>" + count + "</td>";
+                }
+            }
+        }
+        frame += "</tr>";
+    }
+    return frame;
+}
--- JavaScript/change.js
+++ JavaScript/change.js
@@ -2,14 +2,23 @@ const tohome = document.getElementById("tohome");
 const tosetting = document.getElementById("tosetting");
 const home = document.getElementById("home");
 const setting = document.getElementById("setting");
-
-
-tosetting.addEventListener("click",function(){
-    home.classList.add("none");
-    setting.classList.remove("none");
-})
+const calenders = document.getElementById("calenders");
+const tocalenders = document.getElementById("tocalenders");
 
 tohome.addEventListener("click",function(){
     home.classList.remove("none");
     setting.classList.add("none");
+    calenders.classList.add("none");
+})
+
+tosetting.addEventListener("click",function(){
+    home.classList.add("none");
+    setting.classList.remove("none");
+    calenders.classList.add("none");
+})
+
+tocalenders.addEventListener("click",function(){
+    home.classList.add("none");
+    setting.classList.add("none");
+    calenders.classList.remove("none");
 })
