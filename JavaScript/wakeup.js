5b305f7262cca605d7ae74d2295dc3cd651dd9e1
kikura19 <github118703694+kikura19@users.noreply.github.com>
2023-08-20 19:14:38 +0900

    更新したら、すぐに反映されるようにした

--- JavaScript/wakeup.js
+++ JavaScript/wakeup.js
@@ -1,7 +1,7 @@
 const wakeup = document.getElementById("wakeup");
 const registerTime = document.getElementById("registerTime");
 const alert = document.getElementById("alert");
-const week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const week = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 const wtimes = JSON.parse(localStorage.getItem("wtimes")) || {};
 
 for (let day of week) {
@@ -13,8 +13,8 @@ wakeup.innerHTML = displayWakeup();
 
 registerTime.addEventListener("click",function(){  
     TimeSaving(); 
-    displayWakeup(); 
     alertPop();
+    wakeup.innerHTML = displayWakeup();
 });
 
 function alertPop(){
@@ -32,21 +32,16 @@ function TimeSaving(){
         const theDay = document.getElementById(day);
         wtimes[day] = theDay.value;
       }
-    localStorage.setItem("wtimes",JSON.stringify(wtimes));
+    localStorage.setItem("wtimes",JSON.stringify(wtimes)); 
 };
 
 function displayWakeup(){  
-    const wtimes = JSON.parse(localStorage.getItem("wtimes"));
-
     const theday = new Date();
     theday.setDate(theday.getDate() + 1);
-    const DayOfWeek = theday.getDay(); 
-    const week = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];  
+    const DayOfWeek = theday.getDay();  
     const DayOfWeekString = week[DayOfWeek];
-
-    if (wtimes && wtimes[DayOfWeekString]){
-        const wakeTime = wtimes[DayOfWeekString];
-        return `${wakeTime}`;
+    if (wtimes[DayOfWeekString]){
+        return `${wtimes[DayOfWeekString]}`;
     }else{
         return `未設定`;
     }    
